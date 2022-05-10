050aabca2340c0be88d51acea74d3d21b211a927
Mohammed fareed <94236232+mfcoding@users.noreply.github.com>
2022-05-10 13:25:37 +0200

    Update runtime.h

--- runtime/runtime.h
+++ runtime/runtime.h
@@ -8,12 +8,13 @@ typedef char* string;
 int MAX_MALLOC;
 #define new 0
 #define $string ^!new?new_string(new):0; 
-#define ____MALLOC_Err_mgs puts("Memory allocation failed");
 #define new_string(n) (string)malloc(MAX_MALLOC=n*sizeof(char))
+#define ____MALLOC_Err_mgs puts("Memory allocation failed");
+#define EXIT__FAILURE 1
 
 #define append_string(des, src) \
 des=(string)realloc((string)des, MAX_MALLOC=strlen(des)+strlen(src)+1); \
-if(des!=NULL){ strcpy(des+strlen(des), src); }else{____MALLOC_Err_mgs; return 0;}
+if(des!=NULL){ strcpy(des+strlen(des), src); }else{____MALLOC_Err_mgs; return EXIT__FAILURE;}
 
 #define write_string(des, src) \
 if(strlen(des)+strlen(src) < MAX_MALLOC){ strcpy(des+strlen(des), src);}else{ \
@@ -21,17 +22,17 @@ append_string(des, src); }
 
 #define write_string_lim(des, src) \
 if(strlen(des)+strlen(src) < MAX_MALLOC){ strcpy(des+strlen(des), src);} else { \
-if(des!=NULL) { puts("Not enough allocated memory");}} 
+if(des!=NULL) { puts("Not enough allocated memory"); return EXIT__FAILURE;}} 
 
 #define replace_string(des, src) \
 des=(string)realloc((string)des, MAX_MALLOC=strlen(src)+1); \
-if(des!=NULL){ strcpy(des, src); }else{____MALLOC_Err_mgs; return 0;}
+if(des!=NULL){ strcpy(des, src); }else{____MALLOC_Err_mgs; return EXIT__FAILURE;}
 
 int ____c;
 #define slice_string(src, rem) \
 ____c = 0; while(src[____c]!='\0'){ if(!strncmp(src+____c, rem, strlen(rem))){break;} ____c++;} \
 while(src[____c+strlen(rem)]!='\0'){ src[____c]= src[____c+strlen(rem)]; ____c++;} src[____c] = '\0'; \
-src = (string)realloc((string)src, MAX_MALLOC=____c+1); if(src==NULL){____MALLOC_Err_mgs; return 0;}
+src = (string)realloc((string)src, MAX_MALLOC=____c+1); if(src==NULL){____MALLOC_Err_mgs; return EXIT__FAILURE;}
 
 char *ptr;
 #define insert_string(src, ins, i) \
@@ -39,7 +40,7 @@ ____c = i; src = realloc((string)src, MAX_MALLOC=strlen(src)+strlen(ins)+1); \
 if(src!=NULL){ptr = realloc((string)ptr,  MAX_MALLOC-____c); \
 if(ptr!=NULL){strcpy(ptr, src+____c); strcpy(src+____c, ins); \
 strcpy(src+____c+strlen(ins), ptr); \
-delete(ptr); ptr=NULL;}else{____MALLOC_Err_mgs; return 0;}}else{____MALLOC_Err_mgs; return 0;}
+delete(ptr); ptr=NULL;}else{____MALLOC_Err_mgs; return EXIT__FAILURE;}}else{____MALLOC_Err_mgs; return EXIT__FAILURE;}
 
 #define get_string(des) \
 ____c = 0; while(1){ des[____c] = getchar(); \
