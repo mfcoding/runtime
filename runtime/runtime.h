67bf8bfd003490441b470e9a4893795ee012c7a1
Mohammed fareed <94236232+mfcoding@users.noreply.github.com>
2022-05-15 11:39:29 +0200

    Update runtime.h

--- runtime/runtime.h
+++ runtime/runtime.h
@@ -42,14 +42,14 @@ strcpy(src+____c+strlen(ins), ptr); \
 delete(ptr); ptr=NULL;}else{____MALLOC_Err_msg; exit(EXIT__FAILURE);}}else{____MALLOC_Err_msg; exit(EXIT__FAILURE);}
 
 #define reverse_string(src) \
-____c = 0; for(int *temp=new_int(1),*start=new_int(1),*end=new_int(1);;) { \
+for(int *temp=new_int(1),*start=new_int(1),*end=new_int(1);;) { \
 if(temp==NULL||start==NULL||end==NULL) { ____MALLOC_Err_msg; exit(EXIT__FAILURE); } \
-if(____c == 0){*end=strlen(src)-1;____c++;} \
-if(____c != 0){for(*start = 0;;++*start,--*end) { \
+*end=strlen(src)-1;____c++; \
+for(*start = 0;;++*start,--*end) { \
 *temp=src[*start]; \
 src[*start]=src[*end]; \
 src[*end]=*temp; \
-if(*start==*end||*start==*end-1){delete(start);delete(end);delete(temp);break;}}}break;}
+if(*start==*end||*start==*end-1){delete(start);delete(end);delete(temp);break;}}break;}
 
 #define input_string(des) \
 ____c = 0; while(1){ des[____c] = getchar(); \
