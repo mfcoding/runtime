0e14433d1efa8b9f752477940e0d183a2b1a8f66
Mohammed fareed <94236232+mfcoding@users.noreply.github.com>
2022-05-27 10:33:52 +0200

    Update runtime.h

--- runtime/runtime.h
+++ runtime/runtime.h
@@ -7,25 +7,25 @@ int MAX_MALLOC;
 #define new 0
 #define $string ^!new?new_string(new):0; 
 #define new_string(n) (string)calloc(n, MAX_MALLOC=sizeof(char))
-#define ____MALLOC_Err_msg puts("Memory allocation failed");
+#define MALLOC_ERROR_MSG puts("Memory allocation failed");
 #define EXIT__FAILURE 1
 #define EXIT__SUCCESS 0
 
 #define append_string(des, src) \
 des=(string)realloc((string)des, MAX_MALLOC=strlen(des)+strlen(src)+1); \
-if(des!=NULL){ strcpy(des+strlen(des), src); }else{____MALLOC_Err_msg; exit(EXIT__FAILURE);}
+if(des!=NULL){ strcpy(des+strlen(des), src); }else{MALLOC_ERROR_MSG; exit(EXIT__FAILURE);}
 
 #define write_string(des, src) append_string(des, src)
 
 #define replace_string(des, src) \
 des=(string)realloc((string)des, MAX_MALLOC=strlen(src)+1); \
-if(des!=NULL){ strcpy(des, src); }else{____MALLOC_Err_msg; exit(EXIT__FAILURE);}
+if(des!=NULL){ strcpy(des, src); }else{MALLOC_ERROR_MSG; exit(EXIT__FAILURE);}
 
 int ____c;
 #define slice_string(src, rem) \
 ____c = 0; while(src[____c]!='\0'){ if(!strncmp(src+____c, rem, strlen(rem))){break;} ____c++;} \
 while(src[____c+strlen(rem)]!='\0'){ src[____c]= src[____c+strlen(rem)]; ____c++;} src[____c] = '\0'; \
-src = (string)realloc((string)src, MAX_MALLOC=____c+1); if(src==NULL){____MALLOC_Err_msg; exit(EXIT__FAILURE);}
+src = (string)realloc((string)src, MAX_MALLOC=____c+1); if(src==NULL){MALLOC_ERROR_MSG; exit(EXIT__FAILURE);}
 
 char *ptr;
 #define insert_string(src, ins, i) \
@@ -33,12 +33,12 @@ ____c = i; src = realloc((string)src, MAX_MALLOC=strlen(src)+strlen(ins)+1); \
 if(src!=NULL){ptr = realloc((string)ptr,  MAX_MALLOC-____c); \
 if(ptr!=NULL){strcpy(ptr, src+____c); strcpy(src+____c, ins); \
 strcpy(src+____c+strlen(ins), ptr); \
-delete(ptr); ptr=NULL;}else{____MALLOC_Err_msg; exit(EXIT__FAILURE);}}else{____MALLOC_Err_msg; exit(EXIT__FAILURE);}
+delete(ptr); ptr=NULL;}else{MALLOC_ERROR_MSG; exit(EXIT__FAILURE);}}else{MALLOC_ERROR_MSG; exit(EXIT__FAILURE);}
 
 #define input_string(des) \
 ____c = 0; while(1){ des[____c] = getchar(); \
 if(des[____c] == '\n'){des[____c]='\0'; break;} \
-else{____c++;des=realloc((string)des, MAX_MALLOC=____c+1);if(des==NULL){____MALLOC_Err_msg; exit(EXIT__FAILURE);}}}
+else{____c++;des=realloc((string)des, MAX_MALLOC=____c+1);if(des==NULL){MALLOC_ERROR_MSG; exit(EXIT__FAILURE);}}}
 
 #define sizeof_list(s) sizeof(s)/sizeof(s[0])
 
